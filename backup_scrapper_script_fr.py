b02a7692a506a4d0f2719072e7b2b9ef3735658d
Osiris <0e609404e53b251f786b41b7be93cc@gmail.com>
2024-09-15 22:26:01 +0100

    Update backup_scrapper_script_fr.py

--- backup_scrapper_script_fr.py
+++ backup_scrapper_script_fr.py
@@ -3,6 +3,7 @@ print("make by    https://github.com/Osiris-Unnefer\n\n\n\n\n\n\n")
 import subprocess
 #----------------variables------------------
 compteur = 1
+nombre_fichier = 0
 extension0 =''
 extension = '.bak'
 extension1 = '.backup'
@@ -12,13 +13,14 @@ extension4 = '.~'
 extension5 = '.tmp'
 erreurs = ["404","unable"]
 #-------------changement wordlist-----------
-word_list = 'wordlist1.txt' #changeable
-remake = input(f"La wordlist choisie est '{word_list}', voulez vous la changer ? (o/n) ->") 
+word_list = 'wordlist1.txt'
+remake = input(f"La wordlist choisie est '{word_list}', voulez vous la changer ? (o/n) ->")
 if remake == 'o':
-     word_list = input("nouvelle wordlist -> ") #nom de la wordlist / chemin de la wordlist
+     word_list = input("nouvelle wordlist -> ")
 
 #---------------choix de l'extension-----------
-mode = int(input("\n[1] Uniquement .bak (généralement plus courante)\n[2] Uniquement .backup\n[3] Les deux (plus lent)\n[4] Global (.bak/.backup/.old/.swp/.~/.tmp)\n\nMode chosit -> ")) 
+mode = int(input("[0] Aucune extension\n[1] Uniquement .bak (généralement plus courante)\n[2] Uniquement .backup\n[3] Les deux (plus lent)\n[4] Global (.bak/.backup/.old/.swp/.~/.tmp)\n\nMode chosit -> "))
+
 #---------------commandes-----------
 url = input('URL -> ')
 with open(word_list, 'r') as fichier:
@@ -29,15 +31,26 @@ with open(word_list, 'r') as fichier:
             sortie = commande.stdout + commande.stderr
             verification_erreur = any(chaque_erreur in sortie for chaque_erreur in erreurs)
 
+            if mode == 0:
+                commande0 = subprocess.run(["wget", url+ligne_wordlist+extension0], capture_output=True, text=True)
+                sortie0 = commande.stdout + commande.stderr
+                verification_erreur0 = any(chaque_erreur in sortie0 for chaque_erreur in erreurs)
+                if verification_erreur0:
+                    print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension0}, {nombre_fichier} total/totaux trouvé(s) ")
+                    compteur = compteur + 1
+                else: 
+                    print(f"==============================================================\nFichier {ligne_wordlist+extension0} a été téléchargé\n==============================================================")
+                    nombre_fichier = nombre_fichier+1
             if mode == 3:
 #-------------------------------------------------.backup-------------------------------
                 commande1 = subprocess.run(["wget", url+ligne_wordlist+extension1], capture_output=True, text=True)
                 sortie1 = commande.stdout + commande.stderr
                 verification_erreur1 = any(chaque_erreur in sortie1 for chaque_erreur in erreurs)
                 if verification_erreur1:
-                    print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension1} ")
+                    print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension1}, {nombre_fichier} total/totaux trouvé(s)")
                     compteur = compteur + 1
                 else: 
+                    nombre_fichier = nombre_fichier+1
                     print(f"==============================================================\nFichier {ligne_wordlist+extension1} a été téléchargé\n==============================================================")
             if mode == 4:
 #-------------------------------------------------.backup-------------------------------
@@ -45,27 +58,30 @@ with open(word_list, 'r') as fichier:
                 sortie1 = commande.stdout + commande.stderr
                 verification_erreur1 = any(chaque_erreur in sortie1 for chaque_erreur in erreurs)
                 if verification_erreur1:
-                    print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension1} ")
+                    print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension1}, {nombre_fichier} total/totaux trouvé(s) ")
                     compteur = compteur + 1
                 else: 
+                    nombre_fichier = nombre_fichier+1
                     print(f"==============================================================\nFichier {ligne_wordlist+extension1} a été téléchargé\n==============================================================")
 #-------------------------------------------------.old-------------------------------
                 commande2 = subprocess.run(["wget", url+ligne_wordlist+extension2], capture_output=True, text=True)
                 sortie2 = commande.stdout + commande.stderr
                 verification_erreur2 = any(chaque_erreur in sortie2 for chaque_erreur in erreurs)
                 if verification_erreur2:
-                    print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension2} ")
+                    print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension2}, {nombre_fichier} total/totaux trouvé(s) ")
                     compteur = compteur + 1
                 else: 
+                    nombre_fichier = nombre_fichier+1
                     print(f"==============================================================\nFichier {ligne_wordlist+extension2} a été téléchargé\n==============================================================")
 #-------------------------------------------------.swp-------------------------------
                 commande3 = subprocess.run(["wget", url+ligne_wordlist+extension3], capture_output=True, text=True)
                 sortie3 = commande.stdout + commande.stderr
                 verification_erreur3 = any(chaque_erreur in sortie3 for chaque_erreur in erreurs)
                 if verification_erreur3:
-                    print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension3} ")
+                    print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension3}, {nombre_fichier} total/totaux trouvé(s) ")
                     compteur = compteur + 1
                 else: 
+                    nombre_fichier = nombre_fichier+1
                     print(f"==============================================================\nFichier {ligne_wordlist+extension3} a été téléchargé\n==============================================================")
 #-------------------------------------------------extesion ~-------------------------------
 
@@ -73,22 +89,25 @@ with open(word_list, 'r') as fichier:
                 sortie4 = commande.stdout + commande.stderr
                 verification_erreur4 = any(chaque_erreur in sortie4 for chaque_erreur in erreurs)
                 if verification_erreur4:
-                    print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension4} ")
+                    print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension4}, {nombre_fichier} total/totaux trouvé(s) ")
                     compteur = compteur + 1
                 else: 
+                    nombre_fichier = nombre_fichier+1
                     print(f"==============================================================\nFichier {ligne_wordlist+extension4} a été téléchargé\n==============================================================")
 #-------------------------------------------------tmp-------------------------------
                 commande5 = subprocess.run(["wget", url+ligne_wordlist+extension5], capture_output=True, text=True)
                 sortie5 = commande.stdout + commande.stderr
                 verification_erreur5 = any(chaque_erreur in sortie5 for chaque_erreur in erreurs)
                 if verification_erreur5:
-                    print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension5} ")
+                    print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension5}, {nombre_fichier} total/totaux trouvé(s) ")
                     compteur = compteur + 1
                 else: 
+                    nombre_fichier = nombre_fichier+1
                     print(f"==============================================================\nFichier {ligne_wordlist+extension5} a été téléchargé\n==============================================================")
 #-------------------------------------------------.bak-------------------------------
             if verification_erreur:
-                print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension} ")
+                print(f"Essai numéro {compteur} non résultant pour {ligne_wordlist+extension}, {nombre_fichier} total/totaux trouvé(s) ")
                 compteur = compteur + 1
             else: 
+                nombre_fichier = nombre_fichier+1
                 print(f"==============================================================\nFichier {ligne_wordlist+extension} a été téléchargé\n==============================================================")
